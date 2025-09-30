1dad0676fe23fb86e1e5e73f2e315c968b0f0a05
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2025-09-30 17:00:00 +0200

    ejercicio 14 arreglado

--- Tema 2.1/ejercicio14/ejercicio14/src/ejercicio14/Ejercicio14.java
+++ Tema 2.1/ejercicio14/ejercicio14/src/ejercicio14/Ejercicio14.java
@@ -13,18 +13,18 @@ public class Ejercicio14 {
     /**
      * @param args the command line arguments
      */
+    final static float PI = 3.14f;
+    
     public static void main(String[] args) {
-        //Realiza el area de una circunferecia= 2*r² ; de radio 5,2 centimetros. Para ello utiliza la constante PI*/
+        //Realiza el area de una circunferecia= 2*r² hay que poner radio*radio para calcular el radio al cuadrado ; de radio 5,2 centimetros. Para ello utiliza la constante PI*/
         //Muestra por pantalla de igual forma que el ejercicio anterior*/
         float radio;
-        float pi;
-        float resultado;
+        float resultadoArea;
         
         radio = 5.2f;
-        pi = 3.1416f;
-        resultado= 2*radio;
+        resultadoArea= 2*radio*radio;
         
-        System.out.println(" el area de la circunferecia es igual a 2 " + radio + "*" + radio + "²" );
+        System.out.println(" el area de la circunferecia con radio " + radio + "cm es: " + resultadoArea + "cm cuadrados" );
         
         
         
