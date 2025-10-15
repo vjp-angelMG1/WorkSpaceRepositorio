79d8ae6ee671047937ffaa627e2511d106a7fad0
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2025-10-15 15:35:30 +0200

    Revert "cambios"
    This reverts commit 6c9e016ec771765264321ae3591abef01202778b.

--- Tema 3/ejercicio16bucleimpares/src/ejercicio16bucleimpares/Ejercicio16bucleimpares.java
+++ Tema 3/ejercicio16bucleimpares/src/ejercicio16bucleimpares/Ejercicio16bucleimpares.java
@@ -22,9 +22,9 @@ public class Ejercicio16bucleimpares {
         do{ //Bucle 
             if (i>20 && i % 2 == 1) { //si el resto de la division de numero 2 entre 2 es igual 2 o 1 se cumple la condicional por lo que se repite el bucle
                     System.out.print(i+ " - "); //imprimo el valor del numero
-                     j++;//incremento en 1 el valor del j contando apartir de que sea mayor por que vente y es impar 
+                     j++;//incremento en 1 el valor del j contando apartir de 20
                }
-               i++; //incrementa el valor de numero de vueltas
+               i++; //Incrementar el numero 1 para contar los numeros desde 0 da 20 vueltas al bucle
            
         }while (i < 160); // mientras que sea el numero menor o igual a 133 para obtener los numeros pares del 11 a el 133
                System.out.println("cantidad de numero impares : " +i); //imprimo el valor del numero de vueltas impares
