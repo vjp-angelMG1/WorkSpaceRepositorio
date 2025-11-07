c50128eee5ae1ab2814b7dce7fcffba9cdafd6ce
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2025-11-07 20:20:04 +0100

    cambios

--- Tema 5/Coche/src/coche/Coche.java
+++ Tema 5/Coche/src/coche/Coche.java
@@ -9,12 +9,65 @@ package coche;
  * @author alumno
  */
 public class Coche {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    // 1. ATRIBUTOS: características. NUNCA se inicializan aquí.
+    private String ruedas;
+    private String suspension;
+    private int espejos;
+    
+   //2. CONSTRUCTORES: permiter "construir" (instanciar) un objeto de este tipo.
+   // Inicializan todos los atributos.
+   // 2.1. CONSTRUCTOR POR DEFECTO
+   public Coche() {
+       this.ruedas ="";
+       this.suspension ="";
+       this.espejos = 0;
+   }   
+   // 2.2. CONSTRUCTOR PARAMETRIZADO (sobrecarga de métodos y autorreferencia this)
+   public Coche(String ruedas, String suspension, int espejos) {
+       this.ruedas = ruedas;
+       this.suspension = suspension;
+       this.espejos = espejos;
+   }
+   
+   // 3. MÉTODOS:
+   // 3.1 GETTERS Y SETTERS: devuelven y establecen el valor de los atributos
+   public String getRuedas() {
+       return this.ruedas;
+   }
+   
+   public void setRuedas(String ruedas) {
+       this.ruedas = ruedas;    
+   }
+   
+   
+   public String getSuspension() {
+       return this.suspension;
+   }
+   public void setSuspension(String suspension) {
+       this.suspension = suspension;
+   }
+   
+   public int getEspejos() {
+      return this.espejos;
+   }
+   public void setEspejos(int espejos) {
+       this.espejos = espejos;
+   }
+           
+   // 3.2. OTROS MÉTODOS
+   public void acelerar() {
+       System.out.println("Acelerando el coche");
+   }
+   public void girar() {
+       System.out.println("Girando el coche");
+   }
+   public void frenar() {
+       System.out.println("Frenando el coche");
+   }
+   //4. TO STRING: devuelve un String con los datos del objeto
+   @Override
+   public String toString() {
+       return "Mi coche tiene ruedas " + this.ruedas + ", suspension " + this.suspension + " y " + this.espejos + "espejos.";
+   }
     
 }
--- Tema 5/Coche/src/coche/Programacion.java
+++ Tema 5/Coche/src/coche/Programacion.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package coche;
-
+import java.util.Scanner;
 /**
  *
  * @author alumno
  */
 public class Programacion {
     
+    public static void main(String[] args) {
+        //1. Instancio un objeto del tipo que he creado
+        //1.1. Con consstructor parametrizado y estableciendo valores con setters
+        Scanner entrada = new Scanner(System.in);
+        Coche miCoche = new Coche();
+        System.out.println(miCoche.toString());
+        
+        miCoche.setEspejos(5);
+        System.out.println(miCoche.toString());
+        
+        System.out.println(miCoche.getEspejos());
+        
+        miCoche.acelerar();
+        
+        // 1.2. Con constructor parametrizado, inicializando los atributos
+        //con los valores pasados por parámetro
+        Coche tuCoche = new Coche("Michelin", "Baja", 3 );
+        System.out.println(tuCoche.toString());
+        
+        //Esto hace lo mismo que la línea anterior, porque detecta el toString() automáticamente
+        System.out.println(tuCoche);
+        
+        System.out.println(tuCoche.getRuedas());
+        
+        
+    }
 }
