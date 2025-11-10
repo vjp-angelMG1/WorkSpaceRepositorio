9ae36a8a9984b0bbbb4be8bb23fae76076886ea1
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2025-11-10 17:46:08 +0100

    actualización tema 5

---
+++ Tema 5/Coche/src/tema05/Bicicleta.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tema05;
+
+/**
+ *
+ * @author alumno
+ */
+
+
+
+
+
+
+
+public class Bicicleta {
+    //ATRIBUTOS
+    private int ruedas;
+    private String manillar;
+    private String pedales;
+    private String frenos;
+    private boolean estaPedaleando;
+    
+    //CONSTRUCTORES
+    //Por defecto
+    public Bicicleta(){
+        this.ruedas = 0;
+        this.pedales = "";
+        this.manillar = "";
+        this.frenos = "";
+        this.estaPedaleando = false;
+    }
+    public Bicicleta(int ruedas, String manillar, String pedales, String frenos, boolean esraPedaleando) {
+        this.ruedas = ruedas;
+        this.manillar = manillar;
+        this.pedales = pedales;
+        this.frenos = frenos;
+        this.estaPedaleando = estaPedaleando;
+    }
+    
+   // GETTERS Y SETTERS
+   public void  setReudas(int ruedas) {
+       this.ruedas = ruedas;
+   }
+   
+   public int getRuedas(){
+       return this.ruedas;
+   }
+   
+   public void setManillar(int manillar) {
+       this.manillar = manillar;
+   }
+   
+   public int getManillar() {
+       return this.manillar;
+   }
+   
+   public void pedales(int pedales) {
+       
+   }
+  public int 
+
+
+//MÉTODOS
+
+//TO STRING
+@Override
+    public String toString() {
+        return "Mi bicleta tiene " + this.ruedas + " ruedas" +
+            ", un manillar " + this.manillar +
+            ", unos pedales " + this.pedales +
+            ", unos frenos " + this.frenos +
+            " y ahora ¿estoy pedaleando? " + this.estaPedaleando;
+}
+}
