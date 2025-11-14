aebaa4185888dacdb56a59e420ca3119f8dbbfd5
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2025-11-14 19:23:46 +0100

    ciclista

---
+++ Tema 5/Coche/src/tema05/Ciclista.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tema05;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ciclista {
+//ATRIBUTOS
+private String nombre;
+private int dorsal;
+private String tipo;
+
+  
+}
