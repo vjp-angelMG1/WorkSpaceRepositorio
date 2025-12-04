90317153e146c8a386608b4a9a1b02173e86f7ed
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2025-12-04 22:21:05 +0100

    ejercicio 7 tema 6

---
+++ Tema 6/Ejercicio7Transistor/src/ejercicio7transistor/ControlRemoto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio7transistor;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ControlRemoto {
+    public void encender();//Método para encender el dispositivo
+    public void apagar();//Método para apagar el dispositivo
+    public void subirVolumen();//Método para subir el volumen
+    public void bajarVolumen();//Método para bajar el volumen
+    public void cambiarCanal(float canal);//Método para cambiar el canal, recibe un número decimal como parámetro
+    
+}
---
+++ Tema 6/Ejercicio7Transistor/src/ejercicio7transistor/Ejercicio7Transistor.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio7transistor;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio7Transistor {
+
+
+    public static void main(String[] args) {
+               //Objeto que se crea de la clase Televisor
+        Television tv = new Television();
+        
+        System.out.println("--Television--");
+        System.out.println("-Encender la tele-");
+        tv.encender();
+        System.out.println("\n-Mostrar informacion-");
+        tv.mostrarInformacion();//Mostrar estado actual
+        System.out.println("\n-Subir volumen 3 veces-");
+        tv.subirVolumen();
+        tv.subirVolumen();
+        tv.subirVolumen();
+        System.out.println("\n-Mostrar la informacion despues de subir el volumen");
+        tv.mostrarInformacion();//Mostrar el nuevo volumen
+        System.out.println("\n-Cambiar de canal-");
+        tv.cambiarCanal(5);//Cambiar al canal 5
+        System.out.println("\n-Bajar el volumen y apagar la tele-");
+        tv.bajarVolumen();
+        tv.apagar();
+        System.out.println("\n-Mostrar informacion de todo-");
+        tv.mostrarInformacion();//Mostrar estado final
+        
+        //Objeto que se crea de la clase Radio
+        Radio radio = new Radio();
+        
+        System.out.println("\n--Radio--");
+        System.out.println("-Encender la radio-");
+        radio.encender();
+        System.out.println("\n-Mostrar informacion-");
+        radio.mostrarInformacion();//Mostrar estado actual
+        System.out.println("\n-Subir volumen 3 veces-");
+        radio.subirVolumen();
+        radio.subirVolumen();
+        radio.subirVolumen();
+        System.out.println("\n-Mostrar la informacion despues de subir el volumen");
+        radio.mostrarInformacion();//Mostrar el nuevo volumen
+        System.out.println("\n-Cambiar de frecuencia-");
+        radio.cambiarCanal(95.5f);//Cambiar a la frecuencia 95.5
+        System.out.println("\n-Bajar el volumen y apagar la tele-");
+        radio.bajarVolumen();
+        radio.apagar();
+        System.out.println("\n-Mostrar informacion de todo-");
+        radio.mostrarInformacion();//Mostrar estado final
+    }
+}
+    
+    
+
