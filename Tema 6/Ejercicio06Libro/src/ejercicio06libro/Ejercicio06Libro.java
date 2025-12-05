7b889f00aaec710c33ab4176051f59bad36910fb
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2025-12-05 12:31:24 +0100

    ejercicio 6 tema 6

---
+++ Tema 6/Ejercicio06Libro/src/ejercicio06libro/Ejercicio06Libro.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio06libro;
+
+/**
+ * * Para probar el programa, implementa una clase Test en la que
+ * instancies un objeto de cada clase creada anteriormente y
+ * probar los métodos de cada objeto.
+ * 
+ * Despues, utiliza el polimorfismo para crear cada uno de
+ * los objetos a partir de la interfaz Transformable y prueba los
+ * métodos implementados nuevamente.
+ *
+ * @author Angel
+ */
+public class Ejercicio06Libro {
+
+    //Método para probar TODAS las funcionalidades de cualquier objeto Transformable
+    
+    public static void probarFuncionalidad(Transformable objeto){
+        
+        // 1-CONCATENAR
+        System.out.println("Cadenas concatenadas: " + objeto.concatenarTodo());
+        // 2-INICIALES
+        System.out.println("Iniciales: " + objeto.obtenerIniciales());
+        // 3-CONTAR VOCALES
+        objeto.contarVocales(); //el método imprime directamente
+        // 4-OBTENER CADENA MAS LARGA
+        System.out.println("Cadena más larga: " + objeto.obtenerCadenaMasLarga());
+        // 5-BUSCAR CADENA (ejemplo de búsqueda)
+        String busqueda = "va";
+        System.out.println("¿Contiene '" + busqueda + "'? " + 
+                objeto.buscarCadena(busqueda));
+    }
+    public static void main(String[] args) {
+        
+         //Creamos los objetos, primero sin polimorfismo
+        
+        Libro libro1 = new Libro("Reinos olvidados ", " R.A. Salvatore ", " Fantasía");
+        Persona persona1 = new Persona("Pepito ", " Salvador Martínez ", " Salva");
+        
+        System.out.println(libro1.toString());
+        probarFuncionalidad(libro1);
+        System.out.println(persona1.toString());
+        probarFuncionalidad(persona1);
+        
+        //Creamos los objetos usando el polimorfismo
+        
+        //Declaramos una variable del tipo de la Interfaz
+        Transformable objetoTransformable;
+        
+        //objetoTransformable guarda un libro
+        objetoTransformable = new Libro("The Witcher ", " Andrzej Sapkowski ", " Fantasía");
+        probarFuncionalidad(objetoTransformable);
+        objetoTransformable = new Persona("Pablito ", " Clavó un Clavito", " Clavero");
+        probarFuncionalidad(objetoTransformable);
+        
+
+        
+    }
+    
+}
---
+++ Tema 6/Ejercicio06Libro/src/ejercicio06libro/Transformable.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio06libro;
+
+/**
+ *
+ * @author Ángel
+ */
+    public interface Transformable {
+    
+    // Creamos los métodos que estarán obligados a usar la clase Libro y Persona
+    
+    public String concatenarTodo();
+    public String obtenerIniciales();
+    public void contarVocales();
+    public String obtenerCadenaMasLarga();
+    public boolean buscarCadena(String cadenaBusqueda);
+    
+}
+    
+
