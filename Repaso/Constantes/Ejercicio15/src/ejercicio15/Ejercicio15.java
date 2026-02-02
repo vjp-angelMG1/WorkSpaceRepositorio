97b3c80246bfb8354d252d9f8f3237e7191e3355
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-02 21:27:58 +0100

    ejercicio 15 repaso

---
+++ Repaso/Constantes/Ejercicio15/src/ejercicio15/Ejercicio15.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio15;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tiempoTotalEnSegundos = 10000;
+        int tiempoEnHoras = tiempoTotalEnSegundos / 3600;
+        int tiempoRestante = tiempoTotalEnSegundos % 3600;
+        int tiempoEnMinutos = tiempoRestante / 60;
+        int tiempoEnSegundos = tiempoRestante % 60;
+        System.out.println("10.000 segundos hacen un total de: " + tiempoEnHoras + " horas, " + tiempoEnMinutos + " minutos y " + tiempoEnSegundos + " segundos.");
+
+    }
+}
