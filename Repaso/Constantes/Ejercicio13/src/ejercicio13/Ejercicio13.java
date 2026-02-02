51119f2f4ad446398b0e6b224e8301f6c645f247
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-02 20:49:15 +0100

    repaso ejercicio 13

---
+++ Repaso/Constantes/Ejercicio13/src/ejercicio13/Ejercicio13.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio13;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        System.out.println("La variable num1 contiene el valor:" +num1+ " y la variable num2 contiene el valor:"+num2);
+        num1 = 2;
+        num2 = 1;
+        System.out.println("Ahora, la variable num1 contiene el valor:"+num1+ " y la variable num2 contiene el valor"+num2);
+    }
+    
+}
