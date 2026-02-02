a103808b212af89800a434c5d93ed6f2fb970f6d
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-02 20:26:15 +0100

    cambios

---
+++ Repaso/Variables/Ejercicio4Variables/src/ejercicio4variables/Ejercicio4Variables.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio4variables;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio4Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float nota1 = 4.5f;
+        float nota2 = 5.5F;
+        float notaMedia = (nota1+nota2)/2;
+        
+        System.out.println("Programacion");
+        System.out.println("nota del primer examen:"+ nota1);
+        System.out.println("nota del segundo examen:" + nota2);
+        System.out.println("la nota media de la asignatura es:"+notaMedia);
+        
+    }
+    
+}
