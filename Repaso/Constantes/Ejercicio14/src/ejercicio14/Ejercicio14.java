2ff2a451e695497c10a5e48a1ffb50248939a020
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-02 20:57:29 +0100

    repaso ejercicio 14

---
+++ Repaso/Constantes/Ejercicio14/src/ejercicio14/Ejercicio14.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio14;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ejercicio14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static float NUMERO_PI=3.1416f;
+    public static void main(String[] args) {
+        float radio = 5.2f;
+        System.out.println("El area de una circunferencia de radio"+radio+"es : "+NUMERO_PI*(radio*radio));
+    }
+    
+}
