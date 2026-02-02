d540574c609d48c623076c5903f19aa7bcbe424f
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-02 20:40:07 +0100

    repaso ejercicio 9

---
+++ Repaso/Constantes/Ejercicio9/src/ejercicio9/Ejercicio9.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio9;
+
+/**
+ *
+ * @author Angel
+ */
+public class Ejercicio9 {
+
+    /**
+     * 
+     */
+    public static double NUMERO_PI = 3.1416;
+    public static void main(String[] args) {
+        float radio = 3.55f;
+        System.out.println("La longuitud de una circunferencia cuyo radio vale :"+radio+" es igual a:"+radio*radio*NUMERO_PI+" metros");
+       
+    }
+    
+}
