55edc414249f63643dec4f249bd3c5734235d7aa
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-02 18:05:10 +0100

    repaso

---
+++ Repaso/Variables/EjercicioRepaso3/src/ejerciciorepaso3/EjercicioRepaso3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciorepaso3;
+import java.util.Scanner;
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioRepaso3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Salida de datos
+        System.out.println("Introduzca un número:");
+        //Entrada de datos
+        Scanner entrada = new Scanner (System.in);
+        int numero = entrada.nextInt();
+        
+        
+        
+    }
+    
+}
