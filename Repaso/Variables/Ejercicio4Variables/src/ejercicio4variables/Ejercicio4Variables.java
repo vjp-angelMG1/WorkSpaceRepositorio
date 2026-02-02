44dd62f78b202f0799281ff2676d3fd107f29ff6
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-02 18:03:05 +0100

    ejercicios de repaso

---
+++ Repaso/Variables/Ejercicio4Variables/src/ejercicio4variables/Ejercicio4Variables.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio4variables;
+
+/**
+ *
+ * @author Angel
+ */
+public class Ejercicio4Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int edad=32;
+        float altura=1.70f;
+        System.out.print("Angel Montero Gregorio \n");
+        System.out.println("Edad :"+edad);
+        System.out.println("Altura :"+altura);
+    }
+}
