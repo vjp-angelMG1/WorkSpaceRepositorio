20c413ea1272f3a624b906e7809685656317a635
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-02-06 20:01:01 +0100

    cambios ejercicio1

--- Repaso/Tema3/Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Repaso/Tema3/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -3,8 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio1;
-
-import java.util.Scanner;
+ 
+import java.util.Scanner; //importación de la clase java util el paquete scanner
 
 /**
  *
@@ -15,15 +15,15 @@ public class Ejercicio1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int numero1;
-        System.out.println("Introduce un numero");
-        numero1 = entrada.nextInt();
-        if (numero1>0){
-            System.out.println("el numero es positivo");
-        }else if (numero1<0){
-            System.out.println("el numero es negativo");
+    public static void main(String[] args) { //metodo main principal 
+        Scanner entrada = new Scanner(System.in); //creo el scanner
+        int numero1; //Declaro la variable 
+        System.out.println("Introduce un numero"); //imprimo mensaje para introducir numero
+        numero1 = entrada.nextInt(); //guardo el numero recibido por pantalla del usuario
+        if (numero1>0){ //condicional si el numero es mayor que cero
+            System.out.println("el numero es positivo"); //imprimo que el numero es positivo
+        }else if (numero1<0){ //condicional si el numero es menor que cero es negativo
+            System.out.println("el numero es negativo"); //imprimo por pantalla el resultado al darse la condicion
         }
         
     }
