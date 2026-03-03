a5fae18218b7ac6d20276269d286bfdb459fdfea
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-03-03 20:17:36 +0100

    Inacabado tema 4 repaso ejercicio 10

---
+++ Repaso/Tema4/Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio10;
+
+/**
+ * Ejercicio 10.- Escribe un programa en JAVA que, utilizando bucles,
+imprima la tabla de multiplicar de un número que elija el usuario. El
+número se pedirá en un método y la tabla de multiplicar se escribirá
+en otro.
+• Ejemplo:
+Introduzca un numero para calcular su tabla de multiplicar: 8
+8 x 0 = 0
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24 ...
+
+ * @author Ángel
+ */
+public class Ejercicio10 {
+
+ 
+    public static void main(String[] args) {
+       
+           
+       }
+    }
+    
+}
