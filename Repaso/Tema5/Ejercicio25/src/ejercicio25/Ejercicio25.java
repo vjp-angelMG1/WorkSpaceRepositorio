2c7d4e379c31f4aaac25b1882b8fb51b7da47f03
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-03-25 13:59:16 +0100

    Ejercicio 25 tema 5 solo enunciado sin hacer

---
+++ Repaso/Tema5/Ejercicio25/src/ejercicio25/Ejercicio25.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio25;
+
+/**
+ *• Ejercicio 25.- Realiza un programa en JAVA que constará de las siguientes clases:
+Ordenador, Servidor (hereda de Ordenador), Portátil (hereda de Ordenador) y Test
+contiene el metodo main y otros métodos estáticos.
+• La superclase Ordenador contiene los siguientes atributos: cantidad de memoria
+RAM, capacidad del disco duro, modelo de procesador, modelo de tarjeta gráfica y
+precio. A la hora de que el usuario cree un objeto, en la clase Test, deberemos
+controlar que la capacidad del disco duro sea múltiplo de 5, el modelo de
+procesador lo permitirá elegir entre una lista, y el precio siempre sea mayor que 0.
+• La subclase Servidor contiene los siguientes atributos: tamaño del monitor,
+modelo de teclado y modelo de ratón. A la hora de que el usuario cree un objeto,
+en la clase Test, deberemos controlar que el tamaño del monitor sea mayor que 14.
+(en caso de que el usuario no lo cumpla deberemos volvérselo a pedir, tantas veces
+como sea necesario.)
+
+ * @author USUARIO
+ */
+public class Ejercicio25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
