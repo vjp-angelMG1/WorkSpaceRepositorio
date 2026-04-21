f3d48c87d4d5f142a2779bdaeeeee2d11c0db836
vjp-angelMG1 <amonterog04@informatica.iesvalledeljerteplasencia.es>
2026-04-21 16:03:23 +0200

    cambios

--- Tema 12/Ejercicio9/src/ejercicio9/Ejercicio9.java
+++ Tema 12/Ejercicio9/src/ejercicio9/Ejercicio9.java
@@ -4,38 +4,6 @@
  */
 package ejercicio9;
 
-<<<<<<< HEAD
-import java.io.BufferedReader;
-import java.io.IOException;
-
-/**
- *• Ejercicio 09.- Realiza un programa en JAVA que lea un
-archivo creado en el bloc de notas llamado frase.txt que
-contiene una línea de texto. Luego, el programa creará
-un archivo llamado fraseinvertida.txt que contendrá el
-texto invertido del archivo frase.txt.
-• Ejemplo: Si frase.txt contiene Hola, que tal
-fraseinvertida.txt contendrá lat euq ,aloH
-
- * @author Angel
- */
-public class Ejercicio9 {
-        public static String BLOGDENOTAS = "frase.txt";
-    public static void leerFichero(BufferedReader br)throws IOException{
-        String linea;
-        linea = br.readLine();
-        while (linea != null) {
-            System.out.println(linea);
-            linea = br.readLine();
-        }
-        
-    }
-    public static void main(String[] args) {
-        
-    }
-    
-}
-=======
 import java.io.*; //importo todas la clases de los paquetes io y util
 import java.util.*;
 
@@ -182,4 +150,3 @@ public class Ejercicio9 {
     }
 
 }
->>>>>>> c44043bb38efb5081f3369f2c5c8022ec2320de8
